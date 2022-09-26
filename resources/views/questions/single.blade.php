42740b39d96d40aac8f5d2bede94b620cf542428
Jacob George <kooldrjay060@gmail.com>
2022-09-26 11:54:50 +0100

    fixed single question page

--- resources/views/questions/single.blade.php
+++ resources/views/questions/single.blade.php
@@ -34,7 +34,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
                                         </svg>
-                                        <a href="#" class="ml-2 text-sm font-medium text-green-500 hover:text-gray-700" aria-current="page">{{ $question->categories->first()->name }}</a>
+                                        <a href="#" class="ml-2 text-sm font-medium text-green-500 hover:text-gray-700" aria-current="page">{{ $question->categories->first()['name'] }}</a>
                                     </div>
                                 </li>
                                 <li>
